4dca3fc7f601b6fe5dc0eccd4c52a89f41c8f648
Petr Skoda <petr.skoda@openlms.net>
2024-09-19 12:52:21 +0200

    OWORK-1157 fix Moodle 4.5 compatibility

--- version.php
+++ version.php
@@ -27,14 +27,14 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 
-$plugin->version   = 2024090500;
-$plugin->requires  = 2023100902.00; // 4.3.2
+$plugin->version   = 2024091900;
+$plugin->requires  = 2024091700.00; // 4.5.0
 $plugin->component = 'certificateelement_programs';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = 'v3.3.0dev';
-$plugin->supported = [403, 403];
+$plugin->release   = 'v4.0.0dev';
+$plugin->supported = [405, 405];
 
 $plugin->dependencies = [
-    'enrol_programs' => 2024080300,
-    'tool_certificate' => 2024081300,
+    'tool_certify' => 2024091900,
+    'tool_certificate' => 2024090300,
 ];
