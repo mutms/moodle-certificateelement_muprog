eca972a23a3d60ce6bd504ad92bff6cbab0d7e4b
Petr Skoda <petr@mutms.org>
2026-02-12 20:27:24 +0100

    Prepare release

--- version.php
+++ version.php
@@ -28,14 +28,14 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'certificateelement_muprog';
-$plugin->version = 2026020845;
+$plugin->version = 2026021245;
 $plugin->requires = 2024100700;
 $plugin->maturity = MATURITY_BETA;
 $plugin->supported = [405, 405];
 $plugin->incompatible = 500;
-$plugin->release = 'mu-4.5.9-01+';
+$plugin->release = 'v4.5.10.01';
 
 $plugin->dependencies = [
-    'tool_muprog' => 2026020845,
+    'tool_muprog' => 2026021245,
     'tool_certificate' => 2025031800,
 ];
